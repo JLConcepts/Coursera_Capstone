07be39169be63d1b99665c43dd4d8a507486f528
JLConcepts <81524669+JLConcepts@users.noreply.github.com>
2021-03-28 11:15:43 -0500

    Add files via upload

---
+++ Capstone Project Course.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **This is the IBM Data Science Certification Capstone Project
+
+# In[1]:
+
+
+#Week 1 import libraries and print "Hello Capstone Project"
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
